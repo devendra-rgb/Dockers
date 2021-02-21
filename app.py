5559144ad001a13206ec56a41bb52b094eaf2342
devendra-rgb <63838746+devendra-rgb@users.noreply.github.com>
2021-02-21 12:08:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,93 +1,37 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jun 13 02:20:31 2020
-
-@author: Krish Naik
-"""
-
-# -*- coding: utf-8 -*-
-"""
-Created on Fri May 15 12:50:04 2020
-
-@author: krish.naik
-"""
-
-
-import numpy as np
-import pickle
-import pandas as pd
-#from flasgger import Swagger
-import streamlit as st 
-
+import streamlit as st
 from PIL import Image
-
-#app=Flask(__name__)
-#Swagger(app)
-
-pickle_in = open("classifier.pkl","rb")
-classifier=pickle.load(pickle_in)
-
-#@app.route('/')
-def welcome():
-    return "Welcome All"
-
-#@app.route('/predict',methods=["Get"])
-def predict_note_authentication(variance,skewness,curtosis,entropy):
-    
-    """Let's Authenticate the Banks Note 
-    This is using docstrings for specifications.
-    ---
-    parameters:  
-      - name: variance
-        in: query
-        type: number
-        required: true
-      - name: skewness
-        in: query
-        type: number
-        required: true
-      - name: curtosis
-        in: query
-        type: number
-        required: true
-      - name: entropy
-        in: query
-        type: number
-        required: true
-    responses:
-        200:
-            description: The output values
-        
-    """
-   
-    prediction=classifier.predict([[variance,skewness,curtosis,entropy]])
-    print(prediction)
-    return prediction
+import numpy as np
+import cv2
+import tensorflow as tf
 
 
+labels=['Melanocytic nevi',
+'Melanoma',
+'Benign keratosis',
+'Basal cell carcinoma',
+'Actinic keratoses',
+'Vascular lesions',
+'Dermatofibroma']
 
-def main():
-    st.title("Bank Authenticator")
-    html_temp = """
-    <div style="background-color:tomato;padding:10px">
-    <h2 style="color:white;text-align:center;">Streamlit Bank Authenticator ML App </h2>
-    </div>
-    """
-    st.markdown(html_temp,unsafe_allow_html=True)
-    variance = st.text_input("Variance","Type Here")
-    skewness = st.text_input("skewness","Type Here")
-    curtosis = st.text_input("curtosis","Type Here")
-    entropy = st.text_input("entropy","Type Here")
-    result=""
-    if st.button("Predict"):
-        result=predict_note_authentication(variance,skewness,curtosis,entropy)
-    st.success('The output is {}'.format(result))
-    if st.button("About"):
-        st.text("Lets LEarn")
-        st.text("Built with Streamlit")
+def get_output(img):
+    model=tf.keras.models.load_model(r'cancer2.h5')
+    #img=cv2.imread(img)
+    #img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    img=np.resize(img,(28,28))
+    img=img/255.0
+    img=np.reshape(img,(-1,28,28,1))
+    a=model.predict(img)
+    ind=np.argmax(a)
+    label=labels[ind]
+    return label
 
-if __name__=='__main__':
-    main()
-    
-    
-    
+st.title('Skin_Cancer_Detection')
+st.write('By Devendra Tumu')
+upload=st.file_uploader("Upload an jpg Image",type="jpg")
+if upload is not None:
+    image=Image.open(upload)
+    st.image(image,caption="Uploaded Image",use_column_width=True)
+    st.write(" ")
+    st.write("Classifying",)
+    label=get_output(image)
+    st.header("We classified this as {}".format(label))
